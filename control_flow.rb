3867ac35b6e69dce3217db35bddeea1da715b77c
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-28 13:15:22 -0400

    commit tree 2dd952f7fc617156f8e977e28634fd92b7aad424

--- control_flow.rb
+++ control_flow.rb
@@ -25,5 +25,17 @@ end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+"
+    then num1 + num2
+  when "-"
+    then num1 - num2
+  when "*"
+    then num1 * num2
+  when "/"
+    then num1 / num2
+  else puts "Invalid operation!"
+    nil
+  end
 end
 
